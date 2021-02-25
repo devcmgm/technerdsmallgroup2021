95a5c7f1c49d6166077114efc7b0b38bce2eeb3a
devcmgm <49372756+devcmgm@users.noreply.github.com>
2021-02-25 11:43:45 -0600

    k

--- week1and2/src/App.js
+++ week1and2/src/App.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import Routes from "./Routes";
+import { GlobalContext } from "./utils/GlobalContext";
+import "./App.css";
 
-const App = () => {
 
-  return (
-    <div className="App">
-       <h1>Choose Avatar - First Page </h1>
-    </div>
-  )
+const function App() {
+    const user = useState({})
+    useEffect(() => {
+        onLoad();
+    }, []);
+
+    async function onLoad() {
+        try {
+            ;
+        } catch (error) {
+            if (error !== "not identified") {
+                console.log(
+                    "There was an error",
+                    error
+                );
+            }
+        }
+    }
+
+    return (<div className="App">
+            <GlobalContext.Provider value={{user}}>
+                    <Routes />
+                </GlobalContext.Provider>
+            </div>
+    );
 }
 
-export default App
+export default App;
