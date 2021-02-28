5ba404265d721b3d07db0cd5d7d53c2a931f816d
unknown <zachdev2020@gmail.com>
2021-02-28 13:59:33 -0600

    add better vr3

--- week1and2/src/components/FAQ.js
+++ week1and2/src/components/FAQ.js
@@ -38,14 +38,20 @@ class VRScene extends React.Component {
         <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
         <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
-        <Entity text={{value: 'When do you Pray ?', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+        <Entity text={{value: 'When do you Pray ?'}} 
+            position={{x: 0, y: 2, z: -1}}
+            
+        />
 
+        <Entity text={{value: 'In the Morning', }} position={{x: -1, y: 2, z: -1}}/>
+        <Entity text={{value: 'In the Evening', align: 'center'}} position={{x: 0, y: 1, z: -1}}/>
+        <Entity text={{value: 'In the Midday', align: 'center'}} position={{x: 1, y: 2, z: -1}}/>
         <Entity id="box"
           geometry={{primitive: 'box'}}
           material={{color: 'blue', opacity: 0.6}}
           animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
           animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-          position={{x: 0, y: 2, z: -3}}
+          position={{x: 2, y: 2, z: -3}}
           events={{click: this.changeColor.bind(this)}}>
           <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
                   geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
@@ -57,7 +63,7 @@ class VRScene extends React.Component {
           material={{color: this.state.color, opacity: 0.6}}
           animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
           animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-          position={{x: 0, y: 1, z: -3}}
+          position={{x: -2, y: 2, z: -3}}
           events={{click: this.changeColor.bind(this)}}>
           <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
                   geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
@@ -67,11 +73,11 @@ class VRScene extends React.Component {
         <Entity id="box"
           geometry={{primitive: 'box'}}
           material={{color: 'green', opacity: 0.6}}
-          animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-          position={{x: 0, y: 0, z: -3}}
+          animation__rotate={{property: 'rotation', dur: 5, loop: false, to: '360 360 360'}}
+          animation__scale={{property: 'scale', dir: 'alternate', dur: 5, loop: false, to: '1.1 1.1 1.1'}}
+          position={{x: 0, y: 1, z: -3}}
           events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 5, loop: false, to: '2 2 2'}}
                   geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
                   material={{color: '#24CAFF'}}/>
         </Entity>
